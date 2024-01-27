129fa658e2bc6825873df616e902eab1fba32f44
Everus <everuslainus@gmail.com>
2024-01-27 18:28:25 +0530

    increment operator

---
+++ increment.java
@@ -0,0 +1,10 @@
+package Java_Fundamentals;
+
+public class increment {
+    public static void main(String[] args){
+    int age = 20;
+    age++;
+    System.out.println(age);
+    System.out.println(++age);
+    }
+}
