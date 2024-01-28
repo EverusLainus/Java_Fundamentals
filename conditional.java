5b08e186946fcf26b904d8a0e896a6bcecce173c
Everus <everuslainus@gmail.com>
2024-01-28 12:59:53 +0530

    if else

---
+++ conditional.java
@@ -0,0 +1,15 @@
+package Java_Fundamentals;
+
+public class conditional {
+    public static void main(String[] args){
+        int age = 13;
+        if(age >= 18){
+            System.out.println("Eligible to vote");
+
+        }
+        else{
+            System.out.println("Not Eligible to vote");
+        }
+    }
+    
+}
