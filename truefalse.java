77eef849a99281c4c3cbd8bf15406a5e4c83952b
Everus <everuslainus@gmail.com>
2024-01-29 10:11:12 +0530

    true false

---
+++ truefalse.java
@@ -0,0 +1,10 @@
+package Java_Fundamentals;
+
+public class truefalse {
+    public static void main(String[] args){
+        //output of conditional statements
+        System.out.println(2 < 3);
+        System.out.println(3 < 1);
+    }
+    
+}
