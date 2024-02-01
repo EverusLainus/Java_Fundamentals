66b37e541c67416c5ceb66682c8081a4a854bb06
Everus <everuslainus@gmail.com>
2024-02-01 10:58:01 +0530

    arithmetic expression

---
+++ expression.java
@@ -0,0 +1,13 @@
+package Java_Fundamentals;
+
+public class expression {
+    public static void main(String[] args) {
+    int x = 3;
+    int y =5;
+    int z = 10;
+    int a = (++z + y -y +z + x++);
+    System.out.println(a);
+
+
+    }
+}
