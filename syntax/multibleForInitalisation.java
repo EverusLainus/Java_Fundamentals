0f34daca77dba66ad326b3f92229080ab858be69
Everus <everuslainus@gmail.com>
2024-02-02 20:58:37 +0530

    multiple initialisation and increment in for loop

---
+++ syntax/multibleForInitalisation.java
@@ -0,0 +1,11 @@
+package Java_Fundamentals.syntax;
+//multiple initalisation and multiple updatation possibel 
+public class multibleForInitalisation {
+    public static void main(String[] args) {
+        for(int i=1, j=5; i<j; i++, j--){
+            System.out.println("i: "+ i + " j: "+ j);
+
+        }
+    }
+    
+}
